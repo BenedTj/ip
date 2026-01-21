cc0e13ac3c692559ae5246b6cba3c50a7c7d06d8
BenedTj <benedicttjia.prof@gmail.com>
2026-01-21 15:38:11 +0800

    Rewrite Task class to partial solution
    The Task class does not support access to attributes
    by potential subclasses and methods that are shown in
    the partial solution.
    
    Let's:
    * Change the access modifier of attributes to `protected`.
    * Implement the methods `getStatusIcon`, `markAsDone`,
       and `markAsUndone`.
    * Remove the `setIsDone` method.
    
    The `protected` access modifier is used to allow future
    subclasses to access the attributes of the Task class
    directly and setIsDone is removed due to redundancy
    (the same actions can be performed using the `markAsDone`
    and `markAsUndone` methods).

--- src/main/java/ben/task/Task.java
+++ src/main/java/ben/task/Task.java
@@ -2,8 +2,8 @@ package ben.task;
 
 // Class to represent tasks for the list tasks
 public class Task {
-    private String description;
-    private boolean isDone;
+    protected String description;
+    protected boolean isDone;
 
     /**
      * Initializes a Task object with the description and a "false" value
@@ -16,13 +16,32 @@ public class Task {
         this.isDone = false;
     }
 
-    public void setIsDone(boolean isDone) {
-        this.isDone = isDone;
+    /**
+     * Returns "X" if the task is done or whitespace if not
+     *
+     * @return single character string depending on whether the task is done
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    /**
+     * Marks the task as done
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as undone
+     */
+    public void markAsUndone() {
+        this.isDone = false;
     }
 
     @Override
     public String toString() {
         // Only display "X" if this.isDone is true
-        return "[" + (this.isDone ? "X" : "") + "] " + this.description;
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
