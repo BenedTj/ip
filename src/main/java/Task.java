7b3bd96eb0260172828db02902ab533fbddbecb5
BenedTj <benedicttjia.prof@gmail.com>
2026-01-21 11:09:47 +0800

    Create Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
+// Class to represent tasks for the list tasks
 public class Task {
+    private String description;
+    private boolean isDone;
+
+    /**
+     * Initializes a Task object with the description and a "false" value
+     * on isDone
+     *
+     * @param description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        // Only display "X" if this.isDone is true
+        return "[" + (this.isDone ? "X" : "") + "] " + this.description;
+    }
 }
