22106f2f8c4a2b55fa1ec282cff4104d7aed9645
BenedTj <benedicttjia.prof@gmail.com>
2026-01-21 16:16:40 +0800

    Create Todo class

--- src/main/java/ben/task/Todo.java
+++ src/main/java/ben/task/Todo.java
@@ -1,4 +1,19 @@
 package ben.task;
 
-public class Todo {
+// Class to represent tasks without any date or time attached
+public class Todo extends Task {
+    /**
+     * Initializes a Todo object with the description and a "false" value
+     * on isDone
+     *
+     * @param description text description of the Todo task
+     */
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
