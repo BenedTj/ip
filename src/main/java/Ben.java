45b657514d63208db0f286a4a9bc550724946631
BenedTj <benedicttjia.prof@gmail.com>
2026-01-21 12:57:01 +0800

    Rewrite Ben class to use ben.task.Task and add Task commands
    The Ben class uses an array of strings and
    is not extensible to use the commands for the Task class,
    which has commands with parameters and is required for Level-3.
    
    Therefore, the following measures are done:
    * Change `strings` and `stringsLength` to `tasks` and `tasksSize`
       respectively and for `tasks` to be of type `Task[]`.
    * Rename `addElement` to `addTask` and `printElements` to `printTasks`.
    * Rewrite javadoc method comments
       for `addTask` and `printTasks` to clarify
       the usage of the array `tasks` and the variable
       storing its size `tasksSize` and rewrote method bodies
       to use the `Task` class and its methods.
    * Rewrite command identifying logic to instead first
       split user input into command and parameters
       and then use the first word to identify the command.
    * Add `markAndPrintTaskDone` to mark a task as done
       and print a confirmation message.
    * Add `markAndPrintTask` to mark a task as undone
       and print a confirmation message.
    * Add `mark` and `unmark` as recognized commands.
    
    These renaming measures are implemented to ensure that names of
    members of the Ben class are consistent with their intended
    usage and self-explanatory.
    
    The rewriting of the logic to identify
    commands is done to make the codebase more extensible for user
    input which could include parameters that is introduced
    in Level-3.
    
    The rest of the changes are intended to add the task
    marking functionality as required for Level-3.

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -1,10 +1,12 @@
+import ben.task.Task;
+
 import java.util.Scanner;
 
 public class Ben {
     private static final String NAME = "Ben";
     private static final String LINE = "____________________________________________________________";
-    private static String[] strings = new String[100];
-    private static int stringsLength = 0;
+    private static Task[] tasks = new Task[100];
+    private static int tasksLength = 0;
 
     /**
      * Prints a line.
@@ -14,23 +16,63 @@ public class Ben {
     }
 
     /**
-     * Adds a member to the list strings and increments stringsLength
+     * Adds a member to the list tasks and increments tasksLength
      *
-     * @param Element member to add to the list strings
+     * @param Element member to add to the list tasks
      */
-    private static void addElement(String Element) {
-        strings[stringsLength] = Element;
-        stringsLength++;
+    private static void addElement(Task Element) {
+        tasks[tasksLength] = Element;
+        tasksLength++;
     }
 
     /**
-     * Prints all members of the list strings
+     * Prints all members of the list tasks
      */
     private static void printElements() {
-        for (int index = 0; index < stringsLength; index++) {
+        for (int index = 0; index < tasksLength; index++) {
             int currentIndex = index + 1;
-            System.out.println(currentIndex + ". " + strings[index]);
+            System.out.println(currentIndex + ". " + tasks[index]);
         }
+    }
+
+    /**
+     * Set isDone attribute of element of type task
+     * with the index number of indexNumber to true
+     * and prints confirmation message
+     *
+     * @param indexNumber the index number of the element of type task
+     *                    to set isDone attribute
+     */
+    private static void markAndPrintElementDone(int indexNumber) {
+        // Obtain index
+        int index = indexNumber - 1;
+
+        // Set the element as done
+        tasks[index].setIsDone(true);
+
+        // Print message to confirm
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + tasks[index].toString());
+    }
+
+    /**
+     * Set isDone attribute of element of type task
+     * with the index number of indexNumber to false
+     * and prints confirmation message
+     *
+     * @param indexNumber the index number of the element of type task
+     *                    to set isDone attribute
+     */
+    private static void markAndPrintElementUndone(int indexNumber) {
+        // Obtain index
+        int index = indexNumber - 1;
+
+        // Set the element as not done
+        tasks[index].setIsDone(false);
+
+        // Print message to confirm
+        System.out.println("Ok, I've marked this task as not done yet:");
+        System.out.println("  " + tasks[index].toString());
     }
 
     public static void main(String[] args) {
@@ -54,16 +96,33 @@ public class Ben {
         // Perform task
         while (true) {
             String input = scanner.nextLine();
+
+            // Split input into commands and parameters
+            String[] commandParameters = input.split(" ");
+
+            String command = commandParameters[0];
+
             printLine();
-            if (input.equals("bye")) {
+
+            if (command.equals("bye")) {
                 // Exit from loop
                 break;
-            } else if (input.equals("list")) {
+            } else if (command.equals("list")) {
                 // List all elements
                 printElements();
+            } else if (command.equals("mark")) {
+                // Obtain indexNumber and mark done
+                int indexNumber = Integer.parseInt(commandParameters[1]);
+                markAndPrintElementDone(indexNumber);
+            } else if (command.equals("unmark")) {
+                // Obtain indexNumber and mark not done
+                int indexNumber = Integer.parseInt(commandParameters[1]);
+                markAndPrintElementUndone(indexNumber);
             } else {
+                Task newTask = new Task(command);
+
                 // Add to list
-                addElement(input);
+                addElement(newTask);
                 System.out.println("added: " + input);
             }
 
