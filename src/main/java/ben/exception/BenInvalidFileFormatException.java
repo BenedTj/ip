c0e80344efd1e9823b6b2bfa71c2816a8a196612
BenedTj <benedicttjia.prof@gmail.com>
2026-01-27 12:28:22 +0800

    Create BenInvalidFileFormatException

---
+++ src/main/java/ben/exception/BenInvalidFileFormatException.java
@@ -0,0 +1,21 @@
+package ben.exception;
+
+public class BenInvalidFileFormatException extends BenException {
+    /**
+     * Initializes a BenInvalidFileFormatException object
+     * with the default message
+     */
+    public BenInvalidFileFormatException() {
+        super("Invalid line format");
+    }
+    /**
+     * Initializes a BenInvalidFileFormatException object
+     * with the default message that mentions the line
+     * that has the incorrect format
+     *
+     * @param line the content of the line with the incorrect format
+     */
+    public BenInvalidFileFormatException(String line) {
+        super("The following line has an invalid format: " + line);
+    }
+}
