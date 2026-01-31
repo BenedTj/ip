f7e8e388475a1ed7bf57afcae868c51a1dfb2b35
BenedTj <benedicttjia.prof@gmail.com>
2026-02-01 02:52:44 +0800

    Create MarkCommand class

---
+++ src/main/java/ben/core/Command/MarkCommand.java
@@ -0,0 +1,38 @@
+package ben.core.Command;
+
+import ben.core.Storage;
+import ben.core.TaskList;
+import ben.core.Ui;
+import ben.exception.BenIndexOutOfRangeException;
+import ben.exception.BenMarkAlreadyDoneException;
+
+public class MarkCommand extends Command {
+    private int indexNumber;
+
+    /**
+     * Initializes a MarkCommand object
+     * where isExit is initially false
+     * and there is a certain indexNumber.
+     *
+     * @param indexNumber The index number of task to
+     *                    mark done.
+     */
+    public MarkCommand(int indexNumber) {
+        super();
+
+        this.indexNumber = indexNumber;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage)
+            throws BenMarkAlreadyDoneException, BenIndexOutOfRangeException {
+        // Get index
+        int index = indexNumber - 1;
+
+        tasks.markDone(index);
+
+        // Print confirmation message
+        ui.showMessage("Ok, I've marked this task as not done yet:");
+        ui.showMessage("  " + tasks.getTask(index));
+    }
+}
