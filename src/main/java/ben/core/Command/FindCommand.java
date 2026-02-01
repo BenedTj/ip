5e194043aae74151c1faae15a671e1b8098aa4e9
BenedTj <benedicttjia.prof@gmail.com>
2026-02-02 01:39:55 +0800

    Create FindCommand class

--- src/main/java/ben/core/Command/FindCommand.java
+++ src/main/java/ben/core/Command/FindCommand.java
@@ -1,4 +1,41 @@
 package ben.core.Command;
 
-public class FindCommand {
+import ben.core.Storage;
+import ben.core.TaskList;
+import ben.core.Ui;
+import ben.task.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// The class for the find command.
+public class FindCommand extends Command {
+    private String findQuery;
+
+    public FindCommand(String findQuery) {
+        this.findQuery = findQuery;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        // Get array list of tasks
+        ArrayList<Task> taskAsArrayList = tasks.getTasks();
+
+        // Get tasks filtered with findQuery
+        List<Task> filteredTasks = taskAsArrayList
+                .stream()
+                .filter(task -> task.getDescription()
+                        .contains(this.findQuery))
+                .toList();
+
+        // Get length of the filtered list
+        int filteredTasksLength = filteredTasks.size();
+
+        // Print messages
+        ui.showMessage("Here are the matching tasks in your list:");
+        for (int index = 0; index < filteredTasksLength; index++) {
+            int currentIndex = index + 1;
+            ui.showMessage(currentIndex + "." + filteredTasks.get(index));
+        }
+    }
 }
