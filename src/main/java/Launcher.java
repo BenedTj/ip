bd6cc6e71e3ef9b16beefbd16e6675479bfcf104
BenedTj <benedicttjia.prof@gmail.com>
2026-02-11 08:33:26 +0800

    Create Launcher class

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,9 +1,9 @@
-import ben.Ben;
+import ben.Main;
 import javafx.application.Application;
 
 // The class that launches JavaFX
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Ben.class, args);
+        Application.launch(Main.class, args);
     }
 }
