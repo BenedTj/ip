e1a6a99682f739690621bedbbb00997eab122ae1
BenedTj <benedicttjia.prof@gmail.com>
2026-02-19 04:59:19 +0800

    Change title for GUI

--- src/main/java/ben/Main.java
+++ src/main/java/ben/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("Ben");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setBen(this.ben);
             stage.show();
