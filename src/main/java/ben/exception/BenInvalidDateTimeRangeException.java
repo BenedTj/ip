b1f22a1a25f34de703c2d8936cd90f26c7acd0ae
BenedTj <benedicttjia.prof@gmail.com>
2026-02-20 06:37:11 +0800

    Create BenInvalidDateTimeRangeException

---
+++ src/main/java/ben/exception/BenInvalidDateTimeRangeException.java
@@ -0,0 +1,28 @@
+package ben.exception;
+
+import java.time.LocalDateTime;
+
+/**
+ * The class that represents a BenException for an invalid DateTime range.
+ */
+public class BenInvalidDateTimeRangeException extends BenException {
+    /**
+     * Initializes a BenInvalidDateTimeRangeException object
+     * with the default message.
+     */
+    public BenInvalidDateTimeRangeException() {
+        super("The DateTime range is invalid.");
+    }
+
+    /**
+     * Initializes a BenInvalidDateTimeRangeException object
+     * with the starting and ending DateTime value for the range
+     * displayed within the message.
+     *
+     * @param beginDateTime The starting DateTime value for the range.
+     * @param endDateTime The ending DateTime value for the range.
+     */
+    public BenInvalidDateTimeRangeException(LocalDateTime beginDateTime, LocalDateTime endDateTime) {
+        super("The DateTime " + beginDateTime + " must be before " + endDateTime + ".");
+    }
+}
